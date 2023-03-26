2ba555e33153198e779c9911fbf7adfb31dbd6c7
StevanFreeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-03-26 18:13:07 -0500

    feat: implement sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+/**
+ * @method sumAll - Sums all numbers in the given range.
+ * @param {number} start - The start value of the range.
+ * @param {number} end - The end value of the range.
+ * @returns {number} - The sum of the given range.
+ */
+const sumAll = function (start, end) {
+  if (
+    typeof start !== 'number' ||
+    typeof end !== 'number' ||
+    start < 0 ||
+    end < 0
+  ) {
+    return 'ERROR';
+  }
 
+  let result = 0;
+
+  if (start > end) {
+    const temp = start;
+    start = end;
+    end = temp;
+  }
+
+  for (start; start <= end; start++) {
+    result += start;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
