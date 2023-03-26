0a50ab1f5dfbc27b573dd47db89bb2238b515aa8
StevanFreeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-03-26 17:45:59 -0500

    feat: implement repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,29 @@
-const repeatString = function() {
+/**
+ * @method repeatString - repeats a string a given number of times
+ * @param {string} string - string to repeat
+ * @param {number} numberOfRepeats - number of times to repeat the string
+ * @returns {string} - the repeated string
+ */
+const repeatString = function (string, numberOfRepeats) {
+  // using a loop
+  let result = '';
 
+  if (numberOfRepeats < 0) {
+    return 'ERROR';
+  }
+
+  for (const num in [...new Array(numberOfRepeats).keys()]) {
+    result += string;
+  }
+
+  return result;
+
+  // not using a loop
+  // if (numberOfRepeats < 0) {
+  //   return 'ERROR';
+  // }
+
+  // return string.repeat(numberOfRepeats);
 };
 
 // Do not edit below this line
