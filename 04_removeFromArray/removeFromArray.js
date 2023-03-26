70e3818ba889aad4d20daeeb3065c64d6895c3e7
StevanFreeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-03-26 17:58:17 -0500

    feat: implement removeFromArray method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+/**
+ * @method removeFromArray - removes elements from an array
+ * @param {array} array
+ * @param  {...any} values
+ * @returns {array} - the array with the values removed
+ */
+const removeFromArray = function (array, ...values) {
+  return array.filter(element => values.includes(element) === false);
 };
 
 // Do not edit below this line
