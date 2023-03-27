01415a63d6b1e288e7533e98faec3a69a4a2d248
StevanFreeborn <=>
2023-03-26 21:31:49 -0500

    docs: add comments

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
+/**
+* convertToCelsius - Given a temperature in farenheit convert it to celsius.
+* @param {number} tempInFarenheit - The temperature to be converted in farenheit.
+* @returns {number} The temperature converted to celsius.
+*/
 const convertToCelsius = function(tempInFarenheit) {
   return Math.round(
     ((tempInFarenheit - 32) * (5/9)) * 10
   ) / 10;
 };
 
+/**
+* convertToFahrenheit - Given a temperature in celsius convert it to farenheit.
+* @param {number} tempInCelsius - The temperature to be converted in celsius.
+* @returns {number} The temperature converted to farenheit.
+*/
 const convertToFahrenheit = function(tempInCelsius) {
   return Math.round(
     ((tempInCelsius * (9/5)) + 32) * 10
