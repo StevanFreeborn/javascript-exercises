9e40d7a3581864972b722559207c13e01af8a28b
StevanFreeborn <=>
2023-03-26 21:13:57 -0500

    feat: implement leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+/**
+* @method leapYears - Identifies a given year as being a leap year or not.
+* @param {number} year - The year that is to be checked if leap year or not.
+* @returns {boolean} True if the year is a leap year and false otherwise.
+*/
+const leapYears = function(year) {
+  const divisibleBy100 = year % 100 === 0;
+  const divisibleBy400 = year % 400 === 0;
+  const divisibleBy4 = year % 4 === 0;
 
+  return divisibleBy100 && divisibleBy400 || !divisibleBy100 && divisibleBy4;
 };
 
 // Do not edit below this line
