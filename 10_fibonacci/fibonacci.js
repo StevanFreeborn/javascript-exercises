a8f10e35f1ccd2cfdb43f81de0a8bfa932e1637a
StevanFreeborn <=>
2023-03-26 22:48:08 -0500

    feat: implement fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+/**
+* fibonacci - Returns the number in the fibonacci sequence at the given index.
+* @param {number | string} sequenceIndex - The index value for the number you want to return.
+* @returns {number | string} The number in the sequence at the given index. Or a string indicating an error.
+*/
+const fibonacci = function(sequenceIndex) {
+  sequenceIndex = parseInt(sequenceIndex);
 
+  if (sequenceIndex < 0) {
+    return 'OOPS';
+  }
+
+  if (sequenceIndex === 0) {
+    return 0;
+  }
+
+  let prev = 0;
+  let curr = 1;
+
+  while (sequenceIndex > 1) {
+    let temp = prev;
+    prev = curr;
+    curr += temp;
+    sequenceIndex--;
+  }
+
+  return curr;
 };
 
 // Do not edit below this line
