c3875b1740bd85ae4874df67620daa4c7accacae
StevanFreeborn <=>
2023-03-26 21:51:39 -0500

    feat: implement calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,42 @@
-const add = function() {
-	
+const add = function (firstAddend, secondAddend) {
+    return firstAddend + secondAddend;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstSubtrahend, secondSubtrahend) {
+    return firstSubtrahend - secondSubtrahend;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+    return nums.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+    return nums.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+    return Math.pow(base, exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+
+  if (number === 0) {
+    return 1;
+  }
+
+  for (let i = number; i > 1; i--) {
+      number *= i - 1;
+  }
+
+  return number;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
