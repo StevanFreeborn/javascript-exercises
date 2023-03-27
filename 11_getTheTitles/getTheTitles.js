4ad5f082bb8da99abba951017516d352604d82fb
StevanFreeborn <=>
2023-03-26 22:52:29 -0500

    feat: implement get titles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+/**
+* getTheTitles - Returns the titles for all books given.
+* @param {Array<{ title:string, author:string }>} books - The books whose titles we want to get.
+* @returns {Array<string>} The titles of the books given.
+*/
+const getTheTitles = function(books) {
+  return books.map(book => book?.title);
 };
 
 // Do not edit below this line
